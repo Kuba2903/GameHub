828be2ae5cc4a401d993790ff191962fb6e63357
Kuba2903 <jakub.adamczyk@microsoft.wsei.edu.pl>
2024-04-25 17:55:33 +0200

    make the methods asynchronus and overload GetAll with support for pagination

--- Infrastructure/Services/Interfaces/IVideoGames.cs
+++ Infrastructure/Services/Interfaces/IVideoGames.cs
@@ -9,15 +9,24 @@ namespace Infrastructure.Services.Interfaces
 {
     public interface IVideoGames
     {
-        IEnumerable<T> GetAll<T>() where T: class;
+        Task<IEnumerable<T>> GetAllAsync<T>() where T: class;
 
-        T FindById<T>(int id) where T : class;
+        /// <summary>
+        /// Support for pagination
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="pageSize"></param>
+        /// <param name="pageNumber"></param>
+        /// <returns>Entity data</returns>
+        Task<IEnumerable<T>> GetAllAsync<T>(int pageSize, int pageNumber) where T: class;
 
-        void Add<T>(T entity) where T : class;
+        Task<T> FindByIdAsync<T>(int id) where T : class;
 
-        void Remove<T>(T entity) where T : class;
+        Task AddAsync<T>(T entity) where T : class;
 
-        void Update<T>(T entity) where T : class;
+        Task RemoveAsync<T>(T entity) where T : class;
+
+        Task UpdateAsync<T>(T entity) where T : class;
 
     }
 }
