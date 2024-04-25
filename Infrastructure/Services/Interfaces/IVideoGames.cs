64393eab3fbac39a138f87a5200fe08f4d296706
Kuba2903 <jakub.adamczyk@microsoft.wsei.edu.pl>
2024-04-25 17:50:04 +0200

    create interface methods

---
+++ Infrastructure/Services/Interfaces/IVideoGames.cs
@@ -0,0 +1,23 @@
+﻿using Infrastructure.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services.Interfaces
+{
+    public interface IVideoGames
+    {
+        IEnumerable<T> GetAll<T>() where T: class;
+
+        T FindById<T>(int id) where T : class;
+
+        void Add<T>(T entity) where T : class;
+
+        void Remove<T>(T entity) where T : class;
+
+        void Update<T>(T entity) where T : class;
+
+    }
+}
