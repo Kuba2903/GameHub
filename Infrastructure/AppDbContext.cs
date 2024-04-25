aa01d26499a4c2adfbc5a16d6a309e56bf4e9c5d
Kuba2903 <jakub.adamczyk@microsoft.wsei.edu.pl>
2024-04-25 17:27:52 +0200

    create more of sample data

--- Infrastructure/AppDbContext.cs
+++ Infrastructure/AppDbContext.cs
@@ -43,6 +43,9 @@ namespace Infrastructure
                 .HasKey(x => new { x.RegionId, x.Game_PlatformId });
 
 
+            
+            //Create data
+
             modelBuilder.Entity<Genre>()
                 .HasData(new Genre() { Id = 1 ,Genre_Name = "RPG"}, new Genre() {Id = 2 ,Genre_Name = "RTS"},
                 new Genre() {Id = 3 ,Genre_Name = "Shooters"}, new Genre() { Id = 4 ,Genre_Name = "Sports"},
@@ -54,6 +57,71 @@ namespace Infrastructure
                 new Game() { Id = 3 ,GenreId = 3, Game_Name = "Call of Duty" },
                 new Game() { Id = 4 ,GenreId = 4, Game_Name = "Fifa" },
                 new Game() { Id = 5 ,GenreId = 5, Game_Name = "Farming Simulator" });
+
+            modelBuilder.Entity<Publisher>()
+                .HasData(new Publisher() { Id = 1, Publisher_Name = "EA Sports" },
+                new Publisher() { Id = 2, Publisher_Name = "CD Projekt Red"},
+                new Publisher() { Id = 3, Publisher_Name = "Blizzard"},
+                new Publisher() { Id = 4, Publisher_Name = "GIANTS Software"}
+                );
+
+            modelBuilder.Entity<Platform>()
+                .HasData(new Platform() { Id = 1, Platform_Name = "Windows"},
+                new Platform() { Id = 2, Platform_Name = "Play Station 5"},
+                new Platform() { Id = 3, Platform_Name = "Play Station 4"},
+                new Platform() { Id = 4, Platform_Name = "Play Station 3"},
+                new Platform() { Id = 5, Platform_Name = "Play Station 2"},
+                new Platform() { Id = 6, Platform_Name = "Xbox360"},
+                new Platform() { Id = 7, Platform_Name = "Xbox One"},
+                new Platform() { Id = 8, Platform_Name = "Xbox Series X"},
+                new Platform() { Id = 9, Platform_Name = "Android" }
+                );
+
+            modelBuilder.Entity<Game_Publisher>()
+                .HasData(new Game_Publisher() { Id = 1, GameId = 1, PublisherId =  2},
+                new Game_Publisher() { Id = 2, GameId = 2, PublisherId = 3},
+                new Game_Publisher() { Id = 3, GameId = 3, PublisherId = 3},
+                new Game_Publisher() { Id = 4, GameId = 4, PublisherId = 1},
+                new Game_Publisher() { Id = 5, GameId = 5, PublisherId = 4 }
+                );
+
+            modelBuilder.Entity<Region>()
+                .HasData(new Region() { Id = 1, Region_Name = "Europa"},
+                new Region() { Id = 2, Region_Name = "North America"},
+                new Region() { Id = 3, Region_Name = "Asia"},
+                new Region() { Id = 4, Region_Name = "Other" }
+                );
+
+            modelBuilder.Entity<Game_Platform>()
+                .HasData(new Game_Platform() { Id = 1, Game_PublisherId = 1, PlatformId = 1, ReleaseYear = 2015},
+                new Game_Platform() { Id = 2, Game_PublisherId = 1, PlatformId = 3, ReleaseYear = 2015 },
+                new Game_Platform() { Id = 3, Game_PublisherId = 1, PlatformId = 7, ReleaseYear = 2015 },
+                new Game_Platform() { Id = 4, Game_PublisherId = 1, PlatformId = 2, ReleaseYear = 2023 },
+                new Game_Platform() { Id = 5, Game_PublisherId = 1, PlatformId = 8, ReleaseYear = 2023 },
+                new Game_Platform() { Id = 6, Game_PublisherId = 5, PlatformId = 1, ReleaseYear = 2020 },
+                new Game_Platform() { Id = 7, Game_PublisherId = 5, PlatformId = 9, ReleaseYear = 2021 },
+                new Game_Platform() { Id = 8, Game_PublisherId = 2, PlatformId = 1, ReleaseYear = 2000 },
+                new Game_Platform() { Id = 9, Game_PublisherId = 2, PlatformId = 5, ReleaseYear = 2005 },
+                new Game_Platform() { Id = 10, Game_PublisherId = 3, PlatformId = 1, ReleaseYear = 2012 },
+                new Game_Platform() { Id = 11, Game_PublisherId = 3, PlatformId = 4, ReleaseYear = 2013 },
+                new Game_Platform() { Id = 12, Game_PublisherId = 3, PlatformId = 6, ReleaseYear = 2013 },
+                new Game_Platform() { Id = 13, Game_PublisherId = 4, PlatformId = 8, ReleaseYear = 2023 },
+                new Game_Platform() { Id = 14, Game_PublisherId = 4, PlatformId = 2, ReleaseYear = 2023 }
+                );
+
+
+            modelBuilder.Entity<Region_Sales>()
+                .HasData(new Region_Sales() { RegionId = 1, Game_PlatformId = 1, Num_Sales = 90},
+                new Region_Sales() { RegionId = 1, Game_PlatformId = 2, Num_Sales = 30},
+                new Region_Sales() { RegionId = 2, Game_PlatformId = 2, Num_Sales = 40},
+                new Region_Sales() { RegionId = 2, Game_PlatformId = 8, Num_Sales = 60 },
+                new Region_Sales() { RegionId = 3, Game_PlatformId = 8, Num_Sales = 65 },
+                new Region_Sales() { RegionId = 3, Game_PlatformId = 2, Num_Sales = 65 },
+                new Region_Sales() { RegionId = 3, Game_PlatformId = 3, Num_Sales = 200 },
+                new Region_Sales() { RegionId = 3, Game_PlatformId = 7, Num_Sales = 220 },
+                new Region_Sales() { RegionId = 4, Game_PlatformId = 1, Num_Sales = 50 }
+                );
+
         }
     }
 }
---
+++ Infrastructure/Migrations/20240425151832_moreData.cs
@@ -0,0 +1,332 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class moreData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Platforms",
+                columns: new[] { "Id", "Platform_Name" },
+                values: new object[,]
+                {
+                    { 1, "Windows" },
+                    { 2, "Play Station 5" },
+                    { 3, "Play Station 4" },
+                    { 4, "Play Station 3" },
+                    { 5, "Play Station 2" },
+                    { 6, "Xbox360" },
+                    { 7, "Xbox One" },
+                    { 8, "Xbox Series X" },
+                    { 9, "Android" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Publishers",
+                columns: new[] { "Id", "Publisher_Name" },
+                values: new object[,]
+                {
+                    { 1, "EA Sports" },
+                    { 2, "CD Projekt Red" },
+                    { 3, "Blizzard" },
+                    { 4, "GIANTS Software" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Regions",
+                columns: new[] { "Id", "Region_Name" },
+                values: new object[,]
+                {
+                    { 1, "Europa" },
+                    { 2, "North America" },
+                    { 3, "Asia" },
+                    { 4, "Other" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Game_Publishers",
+                columns: new[] { "Id", "GameId", "PublisherId" },
+                values: new object[,]
+                {
+                    { 1, 1, 2 },
+                    { 2, 2, 3 },
+                    { 3, 3, 3 },
+                    { 4, 4, 1 },
+                    { 5, 5, 4 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Game_Platforms",
+                columns: new[] { "Id", "Game_PublisherId", "PlatformId", "ReleaseYear" },
+                values: new object[,]
+                {
+                    { 1, 1, 1, 2015 },
+                    { 2, 1, 3, 2015 },
+                    { 3, 1, 7, 2015 },
+                    { 4, 1, 2, 2023 },
+                    { 5, 1, 8, 2023 },
+                    { 6, 5, 1, 2020 },
+                    { 7, 5, 9, 2021 },
+                    { 8, 2, 1, 2000 },
+                    { 9, 2, 5, 2005 },
+                    { 10, 3, 1, 2012 },
+                    { 11, 3, 4, 2013 },
+                    { 12, 3, 6, 2013 },
+                    { 13, 4, 8, 2023 },
+                    { 14, 4, 2, 2023 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Region_Sales",
+                columns: new[] { "Game_PlatformId", "RegionId", "Num_Sales" },
+                values: new object[,]
+                {
+                    { 1, 1, 90 },
+                    { 2, 1, 30 },
+                    { 2, 2, 40 },
+                    { 8, 2, 60 },
+                    { 2, 3, 65 },
+                    { 3, 3, 200 },
+                    { 7, 3, 220 },
+                    { 8, 3, 65 },
+                    { 1, 4, 50 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Game_Platforms",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Game_Platforms",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Game_Platforms",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Game_Platforms",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Game_Platforms",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Game_Platforms",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Game_Platforms",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Game_Platforms",
+                keyColumn: "Id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Game_Platforms",
+                keyColumn: "Id",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "Region_Sales",
+                keyColumns: new[] { "Game_PlatformId", "RegionId" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "Region_Sales",
+                keyColumns: new[] { "Game_PlatformId", "RegionId" },
+                keyValues: new object[] { 2, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "Region_Sales",
+                keyColumns: new[] { "Game_PlatformId", "RegionId" },
+                keyValues: new object[] { 2, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "Region_Sales",
+                keyColumns: new[] { "Game_PlatformId", "RegionId" },
+                keyValues: new object[] { 8, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "Region_Sales",
+                keyColumns: new[] { "Game_PlatformId", "RegionId" },
+                keyValues: new object[] { 2, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "Region_Sales",
+                keyColumns: new[] { "Game_PlatformId", "RegionId" },
+                keyValues: new object[] { 3, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "Region_Sales",
+                keyColumns: new[] { "Game_PlatformId", "RegionId" },
+                keyValues: new object[] { 7, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "Region_Sales",
+                keyColumns: new[] { "Game_PlatformId", "RegionId" },
+                keyValues: new object[] { 8, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "Region_Sales",
+                keyColumns: new[] { "Game_PlatformId", "RegionId" },
+                keyValues: new object[] { 1, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "Game_Platforms",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Game_Platforms",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Game_Platforms",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Game_Platforms",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Game_Platforms",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Game_Publishers",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Game_Publishers",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Platforms",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Platforms",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Platforms",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Platforms",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Platforms",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Game_Publishers",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Game_Publishers",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Game_Publishers",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Platforms",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Platforms",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Platforms",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Platforms",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Publishers",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Publishers",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Publishers",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Publishers",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
