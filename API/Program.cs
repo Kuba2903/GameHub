f79f47649450bd97461b961168360bcfbcd3a09b
Kuba2903 <jakub.adamczyk@microsoft.wsei.edu.pl>
2024-04-27 15:21:02 +0200

    register services

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,7 @@
+using Infrastructure;
+using Infrastructure.Services.Implementations;
+using Infrastructure.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddSingleton<IVideoGames, VideoGamesService>();
 
 var app = builder.Build();
 
