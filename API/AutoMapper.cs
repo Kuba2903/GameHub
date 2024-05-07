4a49993369fda211cda3a2730c600c3475efe858
Kuba2903 <jakub.adamczyk@microsoft.wsei.edu.pl>
2024-05-07 17:00:36 +0200

    define maping from dto's to entities

--- API/AutoMapper.cs
+++ API/AutoMapper.cs
@@ -9,7 +9,6 @@ namespace API
         public AutoMapper()
         {
            CreateMap<Game,GameDTO>();
-           CreateMap<GameDTO,Game>().ForMember(x => x.Id, opt => opt.Ignore());
            CreateMap<Genre,GenreDTO>();
            CreateMap<Game_Publisher,Game_PublisherDTO>();
            CreateMap<Game_Platform,Game_PlatformDTO>();
@@ -17,6 +16,16 @@ namespace API
            CreateMap<Publisher, PublisherDTO>();
            CreateMap<Region, RegionDTO>();
            CreateMap<Region_Sales, Region_SalesDTO>();
+
+
+           CreateMap<GameDTO, Game>().ForMember(x => x.Id, opt => opt.Ignore());
+           CreateMap<GenreDTO, Genre>().ForMember(x => x.Id, opt => opt.Ignore());
+           CreateMap<Game_PublisherDTO, Game_Publisher>().ForMember(x => x.Id, opt => opt.Ignore());
+           CreateMap<Game_PlatformDTO, Game_Platform>().ForMember(x => x.Id, opt => opt.Ignore());
+           CreateMap<PlatformDTO, Platform>().ForMember(x => x.Id, opt => opt.Ignore());
+           CreateMap<PublisherDTO, Publisher>().ForMember(x => x.Id, opt => opt.Ignore());
+           CreateMap<RegionDTO, Region>().ForMember(x => x.Id, opt => opt.Ignore());
+           CreateMap<Region_SalesDTO, Region_Sales>();
         }
     }
 }
