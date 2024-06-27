82e31f66a2f1109c6f63fd3244512acf35b23c4a
Kuba2903 <jakub.adamczyk@microsoft.wsei.edu.pl>
2024-06-27 11:36:59 +0200

    create index method

--- WebApp/Controllers/GenreController.cs
+++ WebApp/Controllers/GenreController.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Infrastructure.Entities;
+using Infrastructure.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
 
 namespace WebApp.Controllers
 {
     public class GenreController : Controller
     {
-        public IActionResult Index()
+        private readonly IVideoGames service;
+
+        public GenreController(IVideoGames service)
         {
-            return View();
+            this.service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var genres = await service.GetAllAsync<Genre>();
+
+            var query = genres.Select(x => new GenreVm
+            {
+                Id = x.Id,
+                Genre_Name = x.Genre_Name
+            }
+            ).ToList();
+
+            return View(query);
         }
     }
 }
