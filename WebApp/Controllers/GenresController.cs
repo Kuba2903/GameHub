f4228df77dedef6a149b5d6f714a194b5908386d
Kuba2903 <jakub.adamczyk@microsoft.wsei.edu.pl>
2024-06-27 12:05:56 +0200

    make create method

--- WebApp/Controllers/GenresController.cs
+++ WebApp/Controllers/GenresController.cs
@@ -1,6 +1,8 @@
-﻿using Infrastructure.Entities;
+﻿using Infrastructure;
+using Infrastructure.Entities;
 using Infrastructure.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebApp.Models;
 
 namespace WebApp.Controllers
@@ -8,10 +10,11 @@ namespace WebApp.Controllers
     public class GenresController : Controller
     {
         private readonly IVideoGames service;
-
-        public GenresController(IVideoGames service)
+        private readonly AppDbContext dbContext;
+        public GenresController(IVideoGames service, AppDbContext dbContext)
         {
             this.service = service;
+            this.dbContext = dbContext;
         }
 
         public async Task<IActionResult> Index(string sortOrder)
@@ -41,6 +44,34 @@ namespace WebApp.Controllers
             ).ToList();
 
             return View(query);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            GenreVm model = new GenreVm();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(GenreVm model)
+        {
+            var isExisting = await dbContext.Genres.FirstOrDefaultAsync(x => x.Genre_Name == model.Genre_Name);
+
+            if(isExisting == null)
+            {
+                Genre entity = new Genre()
+                {
+                    Genre_Name = model.Genre_Name,
+                };
+                await service.AddAsync<Genre>(entity);
+            }
+            else
+            {
+                return View(model);
+            }
+            
+            return RedirectToAction("Index");
         }
     }
 }
