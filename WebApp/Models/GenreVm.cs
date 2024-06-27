5dc1965979cdd2f830e8f52de8eb14d29964a67f
Kuba2903 <jakub.adamczyk@microsoft.wsei.edu.pl>
2024-06-27 11:31:53 +0200

    create genre viewModel

---
+++ WebApp/Models/GenreVm.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class GenreVm
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Genre name")]
+        public string Genre_Name { get; set; }
+    }
+}
