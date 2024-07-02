5f1dfdc48b78bbcc74e2b3bb239844eb7865d2c9
Kuba2903 <jakub.adamczyk@microsoft.wsei.edu.pl>
2024-07-02 10:17:27 +0200

    create platforms controller

---
+++ WebApp/Controllers/PlatformsController.cs
@@ -0,0 +1,23 @@
+﻿using Infrastructure;
+using Infrastructure.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class PlatformsController : Controller
+    {
+        private readonly IVideoGames _service;
+        private readonly AppDbContext _dbContext;
+
+        public PlatformsController(IVideoGames service, AppDbContext dbContext)
+        {
+            _service = service;
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
