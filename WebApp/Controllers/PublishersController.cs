3dabb7db36c5151c58902f1e5102a43c0c3da643
Kuba2903 <jakub.adamczyk@microsoft.wsei.edu.pl>
2024-07-03 10:38:18 +0200

    create publishers controller, viewmodel, index page

---
+++ WebApp/Controllers/PublishersController.cs
@@ -0,0 +1,23 @@
+﻿using Infrastructure;
+using Infrastructure.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class PublishersController : Controller
+    {
+        private readonly IVideoGames _service;
+        private readonly AppDbContext _dbContext;
+
+        public PublishersController(IVideoGames service, AppDbContext dbContext)
+        {
+            _service = service;
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
