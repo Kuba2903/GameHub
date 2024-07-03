85a0b804e3f7aa6dfbc27a180be4940190ce8dad
Kuba2903 <jakub.adamczyk@microsoft.wsei.edu.pl>
2024-07-03 10:30:32 +0200

    finish platform controller

--- WebApp/Controllers/PlatformsController.cs
+++ WebApp/Controllers/PlatformsController.cs
@@ -2,6 +2,7 @@
 using Infrastructure.Entities;
 using Infrastructure.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebApp.Models;
 
 namespace WebApp.Controllers
@@ -64,6 +65,79 @@ namespace WebApp.Controllers
             {
                 return View(model);
             }
+        }
+
+
+        [HttpGet]
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var item = await _service.FindByIdAsync<Platform>(id);
+
+            if (item != null)
+            {
+                PlatformVm model = new PlatformVm { Id = item.Id, Name = item.Platform_Name };
+                return View(model);
+            }
+            else
+                return View();
+        }
+
+
+        [HttpPost]
+
+        public async Task<IActionResult> Edit(PlatformVm model)
+        {
+            if (ModelState.IsValid)
+            {
+                var entity = await _service.FindByIdAsync<Platform>(model.Id);
+                entity.Platform_Name = model.Name;
+                await _service.UpdateAsync<Platform>(entity);
+
+                return RedirectToAction("Index");
+            }
+            return View(model);
+
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var entity = await _service.FindByIdAsync<Platform>(id);
+
+            if(entity != null)
+            {
+                PlatformVm model = new PlatformVm { Id = entity.Id, Name = entity.Platform_Name };
+                return View(model);
+            }
+            return View();
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> DeleteConfirm(PlatformVm model)
+        {
+            var entity = await _service.FindByIdAsync<Platform>(model.Id);
+            
+            await _service.RemoveAsync<Platform>(entity);
+
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var entity = await _service.FindByIdAsync<Platform>(id);
+
+            var query = _dbContext.Game_Platforms.Include(x => x.Platform).Include(x => x.Game)
+                .Where(x => x.Platform.Platform_Name == entity.Platform_Name).Select(x => x.Game.Game_Name);
+
+            ViewBag.PlatformName = entity.Platform_Name;
+
+            return View(query);
         }
     }
 }
